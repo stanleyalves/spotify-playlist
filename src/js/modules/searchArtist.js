00253b468790ed7a0568ffc7193e832606b21624
Simon Holmes <simonholmes@8rlfl12.chep.com>
2015-06-24 17:24:24 +0100

    fetch

--- src/js/modules/searchArtist.js
+++ src/js/modules/searchArtist.js
@@ -5,7 +5,8 @@ import { ajax, extend } from './utils';
 export default function searchArtist(artist) {
 	return new Promise(function(resolve, reject) {
 		console.log('inside the search artist module')
-		var url = 'https://api.spotify.com/v1/search?q='+ artist +'&type=artist';
+		var url = `https://api.spotify.com/v1/search?q=${artist}&type=artist`;
+
     ajax({
       url: url,
       method:'GET',
@@ -17,3 +18,20 @@ export default function searchArtist(artist) {
     });
 	});
 }
+
+// var Actions = require('../actions/actions');
+// var Promise = require('bluebird')
+// import { ajax, extend } from './utils';
+
+// export default function searchArtist(artist) {
+//   fetch(`https://api.spotify.com/v1/search?q=${artist}&type=artist`)
+//   .then(function(response) {
+//     console.log(response.json());
+//     return response.json()
+//   }).then(function(json) {
+//     console.log('parsed json', json)
+//   }).catch(function(ex) {
+//     console.log('parsing failed', ex)
+//   })
+// }
+
