1a417f6fc942eb56507d81bee74bbf0a4e029463
Simon Holmes <simonholmes@hpl05r1.chep.com>
2015-06-25 17:30:51 +0100

    structure

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 require('babelify/polyfill');
 import 'whatwg-fetch';
-console.log('appJS')
-import { start } from './components/App';
 
+var injectTapEventPlugin = require("react-tap-event-plugin");
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
+import { start } from './components/App';
 start();
